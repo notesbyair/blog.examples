e2e06ba7b0c8561bc3af0a0a8a5fea8a15717356
Ajay Ramesh <ajaycs14@gmail.com>
2021-11-07 06:22:53 -0600

    blog: 2021-11-02-MapStruct-Tutorial

--- src/test/java/com/notesbyair/blog/mapstruct/ApplicationTests.java
+++ src/test/java/com/notesbyair/blog/mapstruct/ApplicationTests.java
@@ -1,10 +1,13 @@
 package com.notesbyair.blog.mapstruct;
 
+import com.notesbyair.blog.mapstruct.example.CarDto;
 import com.notesbyair.blog.mapstruct.mapper.CarMapper;
 import com.notesbyair.blog.mapstruct.example.CarType;
 import com.notesbyair.blog.mapstruct.model.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ApplicationTests {
@@ -15,7 +18,11 @@ class ApplicationTests {
 		car.setMake("Tesla");
 		car.setType(CarType.SPORTS);
 		car.setSeatConfiguration(new SeatConfiguration(1, SeatMaterial.LEATHER));
-		System.out.println(CarMapper.INSTANCE.convert(car));
+		CarDto carDTO = CarMapper.INSTANCE.convert(car);
+		assertEquals(car.getMake(), car.getMake());
+		assertEquals(car.getType(), car.getType());
+		assertEquals(car.getSeatConfiguration().getSeatMaterial().toString(),
+				carDTO.getSeats().getMaterial());
 	}
 
 }
